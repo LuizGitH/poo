27feccd661f9cf7316152f5537f94cb2c550655a
Luiz Felipe Evangelista Assunção <20241050100002@ifg.br>
2025-04-08 21:27:01 -0300

    criacao

---
+++ aula.08.04.2025/SomaSimples.java
@@ -0,0 +1,8 @@
+public class SomaSimples {
+	public static void main(String[] args) { 
+		int a = 5; 
+		int b = 3; 
+		int resultado = a + b; 
+		System.out.println("A doma de 5 e 3 é: " + resultado);
+	}
+}
