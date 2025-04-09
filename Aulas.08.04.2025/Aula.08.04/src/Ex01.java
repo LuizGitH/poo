99f9c7d0c55a9bcd118003eda5a008041aa2aab9
Luiz <luis2016felip@gmail.com>
2025-04-08 22:14:36 -0300

    final

--- Aulas.08.04.2025/Aula.08.04/src/Ex01.java
+++ Aulas.08.04.2025/Aula.08.04/src/Ex01.java
@@ -1,5 +1,16 @@
 public class Ex01 {
     public static void main(String[] args) { //main é o atalho
+        String nome;
+        int idade;
+        double peso;
+
+        nome = "Luiz";
+        idade = 19;
+        peso = 80;
+
+        System.out.println(nome); //sout é o atalho do print
+        System.out.println(idade);
+        System.out.println(peso);
 
     }
 }
---
+++ Aulas.08.04.2025/Aula.08.04/src/Ex02.java
@@ -0,0 +1,15 @@
+public class Ex02 {
+    public static void main(String[] args) {
+        int num_01;
+        int num_02;
+
+        num_01 = 5;
+        num_02 = 7;
+
+        System.out.println("A soma entre os números é " + (num_01 + num_02));
+        System.out.println("A subtração entre os números é " + (num_02 - num_01));
+        System.out.println("A multiplicação entre os números é " + (num_02 * num_01));
+        System.out.println("A divisão entre os números é " + (num_02 / num_01));
+
+    }
+}
